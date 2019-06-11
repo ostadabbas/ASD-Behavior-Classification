fb9fd9f5bdddaa0ff4292d5dbaa768a7c34f0572
Rui Ma <ma.rui@husky.neu.edu>
2019-06-11 22:27:46 +0000

    classification.py edited online with Bitbucket

--- classification.py
+++ classification.py
@@ -3,7 +3,13 @@ import sys
 import os
 import subprocess
 
-
+def parse_args():
+    parser = argparse.ArgumentParser(description='Classification Network')
+    parser.add_argument(
+        '--m', '-m', dest='m', required=True,
+        help='train or test')
+    return parser.parse_args()
+    
 def training(tool_file):
     for f in range(1,6):
         cmd = '''python {tool_file} --f {f}'''.format(tool_file=tool_file,f=f)
@@ -18,11 +24,13 @@ def main(mode):
     if mode=='train':
         tool_file = 'model_cv.py'
         training(tool_file)
-    else:
+    elif mode=='test':
         tool_file = 'prediction.py'
         testing(tool_file)
-
+    else:
+        print('You have to enter train or test')
 
 if __name__ == '__main__':
-    mode='train'
+    args = parse_args()
+    mode = args.m
     main(mode)
